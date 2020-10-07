90cc326ff192fc1a7b26ceb1a67c01c273cea400
adnan-rovcanin <adnan-rovcanin@users.noreply.github.com>
2020-10-07 22:32:45 +0200

    initial commit

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    // Single line comment
+    /*
+        Multiline 
+        comment
+    */ 
+    // Only change code above this line
+}
+console.log(main());
+module.exports = main;
